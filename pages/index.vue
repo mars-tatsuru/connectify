3839bf64fef65f5328fa5453963ce46f5a1fe068
tatsuru koike <t-koike@marsflag.com>
2024-04-02 22:04:48 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// function enableCustomLayout() {
+//   setPageLayout("custom");
+// }
+// definePageMeta({
+//   layout: false,
+// });
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert> This is an auto-imported component </AppAlert>
+    <!-- <button @click="enableCustomLayout">Update layout</button> -->
+  </div>
+</template>
+
+<style lang="scss">
+@use "~/assets/scss/main.scss";
+h1 {
+  color: $primary;
+}
+</style>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const route = useRoute();
+console.log(route.params.id);
+</script>
+
+<template>
+  <div>post</div>
+</template>
