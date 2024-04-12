434481152d8dc3a808b8dcf6436d435b49baf7ff
tatsuru koike <t-koike@marsflag.com>
2024-04-13 00:03:26 +0900

    comment out

--- server/api/microcms.ts
+++ server/api/microcms.ts
@@ -1,13 +1,11 @@
 export default defineEventHandler(async () => {
-  type News = {
-    title?: string;
-    content?: string;
-  };
-
-  const { data } = await useMicroCMSGetList<News>({
-    endpoint: "news",
-    queries: { limit: 10 },
-  });
-
-  return data;
+  // type News = {
+  //   title?: string;
+  //   content?: string;
+  // };
+  // const { data } = await useMicroCMSGetList<News>({
+  //   endpoint: "news",
+  //   queries: { limit: 10 },
+  // });
+  // return data;
 });
