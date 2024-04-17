70886237eb89fb009bf21f80dc7560a3f9d6b250
tatsuru koike <t-koike@marsflag.com>
2024-04-17 10:27:57 +0900

    change fetch side

--- components/TheNews.vue
+++ components/TheNews.vue
@@ -1,11 +1,38 @@
 <script setup lang="ts">
   import type { News } from "@/types/microcms";
   import MainTitle from "@/components/shared/MainTitle.vue";
+  import { createClient } from "microcms-js-sdk";
 
+  /******************************************
+   * use for fetch data in backend
+   *****************************************/
+  // type NewsData = {
+  //   data: News;
+  // };
+
+  /******************************************
+   * use for fetch data in frontend
+   *****************************************/
   type NewsData = {
-    data: News;
+    contents: [
+      {
+        id: string;
+        title: string;
+        content: string;
+        createdAt: string;
+        publishedAt: string;
+        revisedAt: string;
+        updatedAt: string;
+      }
+    ];
+    totalCount: number;
+    offset: number;
+    limit: number;
   };
 
+  /******************************************
+   * variables
+   *****************************************/
   // fetch news data
   const newsData = ref<NewsData | null>(null);
   const offset = ref(0);
@@ -16,38 +43,74 @@
     offset.value = (index - 1) * 5;
   };
 
-  // api fetch
-  const fetchNews = async () => {
-    const { data } = await useFetch("/api/microcms", {
-      params: {
-        endpoint: "news",
-        limit: 5,
-        offset: offset.value,
-      },
-    });
-    newsData.value = data.value as NewsData;
-  };
+  /******************************************
+   * Fetch news data from microCMS in backend
+   *****************************************/
+  // const fetchNews = async () => {
+  //   const { data } = await useFetch("/api/microcms", {
+  //     params: {
+  //       endpoint: "news",
+  //       limit: 5,
+  //       offset: offset.value,
+  //     },
+  //   });
+  //   newsData.value = data.value as NewsData;
+  // };
 
-  // watch offset
-  watch(offset, async () => {
-    await fetchNews();
+  // // watch offset
+  // watch(offset, async () => {
+  //   await fetchNews();
+  // });
+
+  // // Initial fetch
+  // await fetchNews();
+
+  /******************************************
+   * Fetch news data from microCMS in frontend
+   *****************************************/
+  const runtimeConfig = useRuntimeConfig();
+  const client = createClient({
+    serviceDomain: runtimeConfig.public.microcms.domain,
+    apiKey: runtimeConfig.public.microcms.api.key,
   });
 
-  // Initial fetch
-  await fetchNews();
+  // fetch data function
+  const fetchData = async () => {
+    try {
+      const res = await client.get({
+        endpoint: "news",
+        queries: {
+          limit: 5,
+          offset: offset.value,
+        },
+      });
+
+      newsData.value = res as NewsData;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  };
+
+  // initial fetch
+  await fetchData();
+
+  watch(offset, async () => {
+    await fetchData();
+  });
 </script>
 
 <template>
   <div class="news">
     <MainTitle class="title" title="News" />
     <ul class="news-list">
-      <li v-for="item in newsData?.data.contents" class="news-list__item">
+      <li v-for="item in newsData?.contents" class="news-list__item">
         <h2>{{ item.title }}</h2>
         <p>{{ item.content }}</p>
       </li>
     </ul>
     <Pagination
-      :total-count="newsData?.data.totalCount"
+      :total-count="newsData?.totalCount"
       :offset="offset"
       @page-click="onPageChange"
     />
